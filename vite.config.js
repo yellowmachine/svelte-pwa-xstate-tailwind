fcbcd0eaf95b758bf5a79537b15e033d76392911
Yellow Machine <yellow.machine@disroot.org>
2022-07-04 15:43:16 +0200

    added PWA following instructions on https://vite-plugin-pwa.netlify.app/

---
+++ vite.config.js
@@ -0,0 +1,42 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    svelte(),
+    VitePWA({ 
+      manifest: {
+        name: 'Demo PWA',
+        short_name: 'DemoPWA',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png', // <== don't add slash, for testing
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/pwa-512x512.png', // <== don't remove slash, for testing
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png', // <== don't add slash, for testing
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+      strategies: 'injectManifest',
+      srcDir: 'src',
+      filename: 'prompt-sw.ts',
+      registerType: 'prompt',
+      devOptions: {
+        enabled: true
+      }
+    })
+  ]
+})
